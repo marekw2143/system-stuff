95c2e385d6da7c592e94605f4204fb318c56cb78
Marek Wawrzyczek <mwawrzyczek@gmail.com>
2011-05-12 09:18:07 +0200

    added Budzik project for repositories to be cloned

--- clone.py
+++ clone.py
@@ -1,6 +1,6 @@
 import subprocess
 
-REPOSITORY_COUNT = 10
+REPOSITORY_COUNT = 11
 
 names = (
 	'_proto3',
@@ -12,6 +12,7 @@ names = (
 	'mki2',
 	'proxy',
 	'system_stuff',
+    'Budzik',
 	'Kalkulator',
 )
 assert len(names) == REPOSITORY_COUNT
