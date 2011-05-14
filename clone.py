c08c0dcf07b54d7e1cd56df48fa482c76cc3bac3
Marek Wawrzyczek <mwawrzyczek@gmail.com>
2011-05-14 15:29:51 +0200

    changed tabs to white spaces

--- clone.py
+++ clone.py
@@ -3,23 +3,23 @@ import subprocess
 REPOSITORY_COUNT = 12
 
 names = (
-	'_proto3',
-	'binary_keyboard',
-	'comp',
-	'formula_finder',
-	'ip',
+    '_proto3',
+    'binary_keyboard',
+    'comp',
+    'formula_finder',
+    'ip',
     'json_deser',
-	'miarka',
-	'mki2',
-	'proxy',
-	'system_stuff',
+    'miarka',
+    'mki2',
+    'proxy',
+    'system_stuff',
     'Budzik',
-	'Kalkulator',
+    'Kalkulator',
 )
 assert len(names) == REPOSITORY_COUNT
 
 def get_addr_from_name(name):
-	return "git://github.com/marekw2143/%s.git" % name
+    return "git://github.com/marekw2143/%s.git" % name
 
 for name in names:
-	subprocess.call(['git', 'clone', get_addr_from_name(name)])
+    subprocess.call(['git', 'clone', get_addr_from_name(name)])
